6ff9fddb306a1da8e2cae1e0a54e6106a3005b8d
Sahil <sahilbondre@gmail.com>
2020-02-11 22:21:41 +0530

    update node files

--- node/.prettierrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  useTabs: false, // Indent lines with tabs instead of spaces.
-  printWidth: 100, // Specify the length of line that the printer will wrap on.
-  tabWidth: 2, // Specify the number of spaces per indentation-level.
-  singleQuote: false, // Use single quotes instead of double quotes.
-  bracketSpacing: false // Print spaces between brackets in object literals
-};
